ae8bb93dbcb798b8597420eea2489faff3bef402
J4nT <145610618+J4nT@users.noreply.github.com>
2024-02-17 22:57:15 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryArray = galleryItems.map(
+  (item) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`
+);
 
+gallery.innerHTML = galleryArray.join("");
+const lightboxmetods = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
 console.log(galleryItems);
