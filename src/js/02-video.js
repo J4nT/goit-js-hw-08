f3628049b609462d8e74d08d750643def7e6001a
J4nT <145610618+J4nT@users.noreply.github.com>
2024-02-19 22:28:01 +0100

    update task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+const updatecurrentTime = throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000);
+    player.on('timeupdate', updatecurrentTime);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-    player.on('eventName', function(data) {
-  
-});
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(parseFloat(storedTime));
+} 
